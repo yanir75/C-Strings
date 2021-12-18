ce31f4052210c2651f603a832d122d195bdc1884
Yanir <66729230+yanir75@users.noreply.github.com>
2021-12-18 16:39:40 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "dec.h"
+
+
+int main(){
+    char * x = getWord();
+    char * y = getText();
+    check_gem(y,check_sum(x,strlen(x)));
+    atbash(x,y);
+    anagram(x,y,check_sum(x,strlen(x)));
+    free(x);
+    free(y);
+    return 1;
+}
